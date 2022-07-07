55b19b1d7817ddb3f073a8f97fd4bfc10464a8c4
Shubham <shubham@itsbhm.group>
2022-07-07 17:49:07 +0530

    Unplanned Commit #9999

---
+++ prac/main.java
@@ -0,0 +1,11 @@
+public class main {
+  public static void main(String[] a) {
+    
+    String al = new Arraylist();
+    int numArray[]={1,2,3,5,6};
+    Integer iArray = new Integer(4);
+    System.out.println(numArray.getClass());
+    System.out.println(a.getClass().getName());
+    System.out.println("Hello World");
+  }
+}
